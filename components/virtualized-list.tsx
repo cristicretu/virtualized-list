00cf9c9b81ed47652f2aa5181e2a3f5b7a2e989a
Cristian Cretu <crisemcr@gmail.com>
2024-03-30 21:00:13 +0200

    a

--- components/virtualized-list.tsx
+++ components/virtualized-list.tsx
@@ -4,29 +4,47 @@ import { useState } from "react";
 
 const itemHeight = 35;
 const windowHeight = 500;
+const overscan = 10;
 
-export const VirtualizedList = ({
-  itemCount,
-} : { 
-  itemCount: number,
-}) => {
+export const VirtualizedList = ({ itemCount }: { itemCount: number }) => {
   const [scrollTop, setScrollTop] = useState(0);
-  const startIndex = Math.floor(scrollTop / itemHeight);
-  const endIndex = Math.floor((scrollTop + windowHeight) / itemHeight);
+  const startIndex = Math.max(0, Math.floor(scrollTop / itemHeight) - overscan);
+  const endIndex = Math.min(
+    itemCount,
+    Math.ceil((scrollTop + windowHeight) / itemHeight) + overscan
+  );
 
-  const listItems = Array.from({ length: itemCount }, (_, index) => listItem(index));
+  const generateRows = () => {
+    const rows = [];
+    for (let index = startIndex; index < endIndex; index++) {
+      rows.push(listItem(index));
+    }
+    return rows;
+  };
 
   return (
-    <ul style={{ height: windowHeight, overflowY: 'scroll' }} className="overflow-y-scroll w-full border-2 border-black relative">
-      {listItems}
+    <ul
+      style={{ height: windowHeight, overflowY: "scroll" }}
+      className="overflow-y-scroll w-full border-2 border-black relative"
+      onScroll={(e) => setScrollTop(e.currentTarget.scrollTop)}
+    >
+      {generateRows()}
     </ul>
-  )
-}
+  );
+};
 
 const listItem = (index: number) => {
   return (
-    <li style={{ height: itemHeight }} key={index}>
+    <li
+      style={{
+        height: `${itemHeight}px`,
+        top: `${itemHeight * index}px`,
+        backgroundColor: index & 1 ? "#0f0f0f" : "#1f1f1f",
+      }}
+      key={index}
+      className="absolute"
+    >
       List item {index}
     </li>
-  )
-}
+  );
+};
